777d51d0de7059db62c4b4a03b1b6fbce3505f54
shamel jij <shamel.jij@gmail.com>
2022-09-12 09:58:28 +0200

    getBooksbyTitle hinzugefügt

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -10,7 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
 import java.net.http.HttpResponse;
@@ -64,6 +67,5 @@ public class BookController {
     public String hello(@PathParam("name") String name) {
         return "Hello World " + name;
     }
-
 
 }
